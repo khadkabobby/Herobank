d3def469aca686db9bb7304cf4d8b2d1841740a2
bobby khadka <khadkaasmith2059@gmail.com>
2023-04-29 13:17:11 +0545

    business section completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles }) => {
+  return (
+    <button
+      className={`py-4 px-6 text-primary bg-blue-gradient font-poppins font-medium text-[18px] outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
 };
 
 export default Button;
